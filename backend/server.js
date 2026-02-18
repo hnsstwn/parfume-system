8805afee26b8a01edf950743ef2a6f6a307f4a8d
hnsstwn <hanissetiawan8@gmail.com>
2026-02-18 23:14:45 +0700

    add test-db route

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: 'API is running 🚀' });
 });
+const db = require('./config/db');
 
+app.get('/test-db', async (req, res) => {
+    try {
+        const result = await db.query('SELECT NOW()');
+        res.json({
+            success: true,
+            time: result.rows[0]
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            success: false,
+            error: error.message
+        });
+    }
+});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
