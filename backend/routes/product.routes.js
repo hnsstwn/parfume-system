4061a3c62b03491f2492b4151666294eacfa78e0
hnsstwn <hanissetiawan8@gmail.com>
2026-02-18 23:38:16 +0700

    fix conflict product route clean

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-<<<<<<< HEAD
     res.json({ message: 'Product route working' });
-=======
-    res.json({ message: "Product route working" });
->>>>>>> 0b1fa52e4a897106d0a7a87a8c484396d95aa7b4
 });
 
 module.exports = router;
